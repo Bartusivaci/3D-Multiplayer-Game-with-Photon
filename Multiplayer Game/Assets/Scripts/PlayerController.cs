0714a0a17cd9a290c6e27ea97c4413fd9cc6b7a4
Bartusivaci <bartusivaci@gmail.com>
2022-04-01 23:15:30 +0300

    Player is now able to look around.

---
+++ Multiplayer Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] Transform viewPoint;
+    [SerializeField] float mouseSensitivity = 1f;
+    [SerializeField] bool invertLook = false;
+
+    float verticalRotationStore;
+    Vector2 mouseInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensitivity;
+
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + mouseInput.x, transform.rotation.eulerAngles.z);
+
+        verticalRotationStore += mouseInput.y;
+        verticalRotationStore = Mathf.Clamp(verticalRotationStore, -60f, 60f);
+
+        if (invertLook)
+        {
+            viewPoint.rotation = Quaternion.Euler(verticalRotationStore, viewPoint.rotation.eulerAngles.y, viewPoint.rotation.eulerAngles.z);
+        }
+        else
+        {
+            viewPoint.rotation = Quaternion.Euler(-verticalRotationStore, viewPoint.rotation.eulerAngles.y, viewPoint.rotation.eulerAngles.z);
+        }
+
+        
+    }
+}
